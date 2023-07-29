8ac22e08798e407f181b3c551a1365118d6d4824
Mardgelle Neri <mardgelleneri@gmail.com>
2023-07-29 19:03:34 +0100

    database created, info generated, faker gem added to gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  article = Article.new
+  article.title = Faker::Movies::HowToTrainYourDragon.character
+  article.content = Faker::Quote.matz
+  article.save
+end
