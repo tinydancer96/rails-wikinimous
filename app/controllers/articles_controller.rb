9455aece78ef6f26b3c2e0a3b534b724b418f712
Mardgelle Neri <mardgelleneri@gmail.com>
2023-07-31 23:10:49 +0100

    create new article been added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,31 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_path(@article)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to articles_path(@article)
+  end
+
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
